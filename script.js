7bd7061c86be0206800d87ef993765345e41558f
Matheus Alves <matheus.aalves@hotmail.com>
2021-10-26 19:15:15 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ const listaTarefas = document.querySelector('#lista-tarefas'); // ol
 criarTarefa.addEventListener('click', function () {
   let conteudo = textoTarefa.value;
   let listItem = document.createElement('li');
-  listItem.className = 'list-item'
+  listItem.className = 'list-item';
   listItem.appendChild(document.createTextNode(conteudo));
   listaTarefas.appendChild(listItem);
   document.getElementById('texto-tarefa').value = '';
+  
+  const itemLista = document.querySelectorAll('li');
+  for (index = 0; index < itemLista.length; index += 1){
+    itemLista[index].addEventListener('click', function () {
+      console.log('teste');
+      listItem.className = 'selected';
+    });
+  }
 });
 // Source: https://stackoverflow.com/questions/17773938/add-a-list-item-through-javascript
 
