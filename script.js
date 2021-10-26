bd0deb4d4ed2592cc40b515f2bbd91331924c424
Matheus Alves <matheus.aalves@hotmail.com>
2021-10-26 16:32:23 -0300

    Resolvendo requisito 5

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const textoTarefa = document.querySelector('#texto-tarefa');
-const criarTarefa = document.querySelector('#criar-tarefa');
+const textoTarefa = document.querySelector('#texto-tarefa'); // Input
+const criarTarefa = document.querySelector('#criar-tarefa'); // Botão
+const listaTarefas = document.querySelector('#lista-tarefas'); // ol
+
+criarTarefa.addEventListener('click', function () {
+  let conteudo = textoTarefa.value;
+  let listItem = document.createElement('li');
+  listItem.appendChild(document.createTextNode(conteudo));
+  listaTarefas.appendChild(listItem);
+});
+// Source: https://stackoverflow.com/questions/17773938/add-a-list-item-through-javascript
+
